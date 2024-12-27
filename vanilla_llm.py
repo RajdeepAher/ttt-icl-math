d9ab4119448b4cd40823d654e447cd576808d1c2
Aayan Yadav <ydvaayan@gmail.com>
2024-12-27 17:30:56 +0530

    vanilla llm code

---
+++ vanilla_llm.py
@@ -0,0 +1,68 @@
+import json
+import torch
+import numpy as np
+import faiss
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+import evaluate
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+def main():
+    model_name = 'meta-llama/Llama-3.1-8B-Instruct'
+    num_samples = 10
+    results = []
+    eval_dataset_name='HuggingFaceH4/MATH-500'
+    quantization_config = BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    bnb_4bit_quant_type="nf4",
+                    bnb_4bit_compute_dtype=torch.float16,
+                    bnb_4bit_use_double_quant=True,
+                )
+    model = AutoModelForCausalLM.from_pretrained(
+                    model_name,
+                    quantization_config=quantization_config,
+                    device_map="auto",
+                )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    eval_data = load_dataset(eval_dataset_name)['test']
+    if num_samples:
+        eval_data = eval_data.select(range(min(num_samples, len(eval_data))))
+
+    print(f"Evaluating on {len(eval_data)} samples from MATH-500...")
+    check =0
+    for item in tqdm(eval_data):
+        query = item.get('problem', None)
+
+        prompt ="Output <|eot_id|> at the end of final solution. Use \boxed{} only once in each solution, only for the final answer of the asked question."
+        prompt += f"Now solve this problem:\n{query}\nSolution:"
+
+        inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=4096).to(model.device)
+        outputs = model.generate(
+            **inputs,
+            max_length=4096,
+            eos_token_id=tokenizer.eos_token_id,
+            num_return_sequences=1
+        )
+
+
+        generated_solution =tokenizer.decode(outputs[0][len(inputs['input_ids'][0]):], skip_special_tokens=True)
+        if check ==0:
+        print('QUERY--------------')
+        print(query)
+        print('GENERATED SOLUTION--------------')
+        print(generated_solution)
+        print('GROUND TRUTH--------------')
+        print(item.get('solution', ''))
+        check +=1
+        results.append({
+                        'query': query,
+                        'prediction': generated_solution,
+                        'ground_truth': item.get('solution', ''),
+                    })
+    with open('evaluation_results_vanilla.json', 'w') as f:
+    json.dump(results, f, indent=4)
+
+
+if __name__=='__main__':
+    main()
